d1b3bbb993f9c55bc21889761137d126339ec78c
Attiq Rao <attiqrao@Attiqs-MacBook-Pro.local>
2020-01-14 01:07:58 +0500

    session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_station_session'
+Rails.application.config.session_store :cookie_store, key: "_eqipia_session_#{Rails.env}", domain: :all
