77ec82124d170261d5ff805398f69aa12a4d6716
Attiq Rao <attiqrao@Attiqs-MacBook-Pro.local>
2020-01-14 01:22:07 +0500

    added jquery rails gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,2 @@
 class ApplicationController < ActionController::Base
-  #has to be after auto_session_timeout so that prepend will not be overwritten.
-  protect_from_forgery with: :exception, prepend: true
 end
